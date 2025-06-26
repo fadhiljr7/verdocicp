83f098ab0ee8da00e733d985065bf6d06a706393
Muhammad Fadhil Abidin <48880516+fadhiljr7@users.noreply.github.com>
2025-06-26 19:06:38 +0700

    Update backend_id.js

--- src/backend_id.js
+++ src/backend_id.js
@@ -2,4 +2,4 @@
 //export const canisterId = "uzt4z-lp777-77774-qaabq-cai"
 
 //mainnet
-export const canisterId = "gh33k-4qaaa-aaaap-qp4fq-cai"
+export const canisterId = "ganti dengan canister id kamu"
